371cad91db7dade5496e366455c7b090433f9c86
root <root@guifiserv.(none)>
2012-01-09 10:18:05 +0100

    Version V3

---
+++ Scripts/script_diari_v3.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+if [ $# -lt 1 ] || [ $# -gt 2 ]
+then
+     echo
+     echo "Falta configurar els parametres !"
+     echo "Us: $0 fitxer_config.ini [dd/mm/yyyy]"
+     echo
+     exit 1
+fi
+source $1
+if [ $UNCONFIG == 1 ]
+then
+     echo
+     echo "Abans has de configurar el $1"
+     echo
+     exit 1
+fi
+                                          
+if [  ${#USUARIO[@]} != ${#TELF[@]} ] || [ ${#USUARIO[@]} != ${#TARIFA[@]} ]
+then
+     echo "El fitxer de configuració no és correcte, hi ha d'haver el mateix nombre d'usuaris, que de telefons, que de tarifes !!"
+     exit 1
+fi
+# Restem un dia a la fetxa per calcular el consum el dia anterior.
+DATEF=`date -d "-1 day" +%d/%m/%Y`
+
+# Borrem el fitxer d'informe anterior si hi es.
+
+if [ -f $RUTA/informe_diari.txt ]
+then
+rm $RUTA/informe_diari.txt
+fi
+
+# Si li passem un parametre, el fara servir com a fecha.
+# Us: $0 dd/mm/yyyy
+
+if [ $# -eq 2 ]
+     then
+     DATEF=$2
+fi
+
+# Encapçalament Correu i assumpte
+echo MIME-Version: 1.0 >> $RUTA/informe_diari.txt
+echo Content-type: text\/html\; charset=utf-8 >> $RUTA/informe_diari.txt
+echo subject: Informe DIARI de consum el dia $DATEF >> $RUTA/informe_diari.txt
+echo >> $RUTA/informe_diari.txt
+echo \<html\> >> $RUTA/informe_diari.txt
+
+echo
+echo Informe de consum diari en fecha: $DATEF
+echo Informe de consum diari en fecha: $DATEF \<br\> >> $RUTA/informe_diari.txt
+echo
+echo \<br\> >> $RUTA/informe_diari.txt
+
+LINEAS=${#TARIFA[@]}
+for ((i=0;i<LINEAS;i++)); do
+     # Consulta del moviln
+     echo Movil ${USUARIO[$i]}
+     echo
+     echo "************************************" \<br\> >> $RUTA/informe_diari.txt
+     echo \<b\>Movil de ${USUARIO[$i]} \<\/b\> Tarifa Actual: ${TARIFA[$i]}\<br\> >> $RUTA/informe_diari.txt
+     echo "************************************" \<br\> >> $RUTA/informe_diari.txt
+     echo
+     echo \<br\> >> $RUTA/informe_diari.txt
+     /usr/bin/python $RUTA2/$PROGRAMA -i $DATEF -f $DATEF -n ${TELF[$i]} >> $RUTA/informe_diari.txt
+     echo \<br\> >> $RUTA/informe_diari.txt
+
+     echo
+     echo >> $RUTA/informe_diari.txt
+done
+# Enviament de correu electronic
+NUMMAILS=${#CORREO[@]}
+for ((i=0;i<NUMMAILS;i++)); do
+	cat $RUTA/informe_diari.txt | /usr/sbin/sendmail ${CORREO[$i]}
+done
+exit 0
+
---
+++ Scripts/script_mensual_v3.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+if [ $# -lt 1 ] || [ $# -gt 3 ] || [ $# -eq 2 ]
+then
+     echo
+     echo "Falta configurar els parametres !"
+     echo "Us: $0 fitxer_config.ini [dd] [mm/yyyy]"
+     echo
+     exit 1
+fi
+source $1
+if [ $UNCONFIG == 1 ]
+then
+     echo
+     echo "Abans has de configurar el $1"
+     echo
+     exit 1
+fi
+if [  ${#USUARIO[@]} != ${#TELF[@]} ] || [ ${#USUARIO[@]} != ${#TARIFA[@]} ]
+then
+     echo "El fitxer de configuració no és correcte, hi ha d'haver el mateix nombre d'usuaris, que de telefons, que de tarifes !!"
+     exit 1
+fi
+# Restem un dia a la fetxa per calcular el consum el dia anterior.
+DATEF=`date -d "-1 month" +%m/%Y`
+
+DATEVAR=`date +%m`
+if [ $DATEVAR == "01" ]
+then
+    ANY=`date -d "-1 year" +%Y`
+else
+ANY=`date +%Y`
+fi
+
+# Calculem els dies que te el mes anterior
+
+case "$DATEF" in
+   01/$ANY | 03/$ANY | 05/$ANY | 07/$ANY | 08/$ANY | 10/$ANY | 12/$ANY )
+   DIA=31
+   ;;
+   02/$ANY)
+   DIA=28
+   ;;
+   04/$ANY | 06/$ANY | 09/$ANY | 11/$ANY )
+   DIA=30 ;;
+esac
+
+# Borrem el fitxer d'informe anterior si hi es.
+
+if [ -f $RUTA/informe_mensual.txt ]
+then
+rm $RUTA/informe_mensual.txt
+fi
+
+# Si li passem dos parametres, els fara servir com a fecha.
+# Us: $0 dd mm/yyyy
+## dd el maxim de dies del mes
+if [ $# -eq 3 ]
+        then
+        DIA=$2
+        DATEF=$3
+fi
+                
+# Encapçalament Correu i assumpte
+echo MIME-Version: 1.0 >> $RUTA/informe_mensual.txt
+echo Content-type: text\/html\; charset=utf-8 >> $RUTA/informe_mensual.txt
+echo subject: Informe MENSUAL de consum del dia 01/$DATEF al $DIA/$DATEF >> $RUTA/informe_mensual.txt
+echo >> $RUTA/informe_mensual.txt
+echo \<html\> >> $RUTA/informe_mensual.txt
+
+
+echo
+echo Informe de consum Mensual mes/any: $DATEF
+echo Informe de consum Mensual mes/any: $DATEF \<br\> >> $RUTA/informe_mensual.txt
+echo
+echo \<br\> >> $RUTA/informe_mensual.txt
+
+LINEAS=${#TARIFA[@]}
+for ((i=0;i<LINEAS;i++)); do
+	# Consulta del moviln
+	echo Movil ${USUARIO[$i]}
+	echo "************************************" \<br\> >> $RUTA/informe_mensual.txt
+	echo \<b\>Movil de ${USUARIO[$i]}\<\/b\> Tarifa Actual: ${TARIFA[$i]}\<br\> >> $RUTA/informe_mensual.txt
+	echo "************************************" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} >> $RUTA/informe_mensual.txt
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo Si s\'hagues fet servir la tarifa  \<b\>\<font color=\"blue\"\>VIP\<\/font\>\<\/b\> \(sense establiment\) el gasto hauria sigut de: \<br\> >> $RUTA/informe_mensual.txt
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} -m pepe_vip_noprint >> $RUTA/informe_mensual.txt
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo Si s\'hagues fet servir la tarifa \<b\>\<font color=\"blue\"\>4.5 cent\<\/font\>\<\/b\>el gasto hauria sigut de: \<br\> >> $RUTA/informe_mensual.txt
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} -m pepe5_noprint >> $RUTA/informe_mensual.txt
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	
+	echo
+	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo Si s\'hagues fet servir la tarifa  \<b\>\<font color=\"blue\"\>Pulpo Paul\<\/font\>\<\/b\> el gasto hauria sigut de: \<br\> >> $RUTA/informe_mensual.txt
+	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} -m pepe_pulpo_noprint >> $RUTA/informe_mensual.txt
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo Si s\'hagues fet servir la tarifa \<b\>\<font color=\"blue\"\>RATONCITO Y ELEFANTE\<\/font\>\<\/b\> el gasto hauria sigut de: \<br\> >> $RUTA/informe_mensual.txt
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} -m pepe_cobaya_noprint >> $RUTA/informe_mensual.txt	
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo Si s\'hagues fet servir la tarifa \<b\>\<font color=\"blue\"\>NUEVEGA Y HABLA\<\/font\>\<\/b\> el gasto hauria sigut de: \<br\> >> $RUTA/informe_mensual.txt
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} -m pepe_nuevega_noprint >> $RUTA/informe_mensual.txt	
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo Si s\'hagues fet servir la tarifa \<b\>\<font color=\"blue\"\>MOVIL TODAY\<\/font\>\<\/b\> el gasto hauria sigut de: \<br\> >> $RUTA/informe_mensual.txt
+	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	/usr/bin/python $RUTA2/$PROGRAMA -i 01/$DATEF -f $DIA/$DATEF -n ${TELF[$i]} -m pepe_movil_today_noprint >> $RUTA/informe_mensual.txt	
+	echo \<br\> >> $RUTA/informe_mensual.txt
+	echo
+	
+	
+	echo \<\/html\> >> $RUTA/informe_mensual.txt
+	echo
+done	
+# Enviament de correu electronic
+NUMMAILS=${#CORREO[@]}
+for ((i=0;i<NUMMAILS;i++)); do
+	cat $RUTA/informe_mensual.txt | /usr/sbin/sendmail ${CORREO[$i]}
+done
+exit 0
